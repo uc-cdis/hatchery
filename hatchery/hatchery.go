24f4486d2e288ff7ff9a573db2cf855406a92c3a
Jawad Qureshi <qureshi@uchicago.edu>
2021-06-24 16:50:00 -0500

    Add /paymodels endpoint

--- hatchery/hatchery.go
+++ hatchery/hatchery.go
@@ -17,6 +17,7 @@ func RegisterHatchery() {
 	http.HandleFunc("/terminate", terminate)
 	http.HandleFunc("/status", status)
 	http.HandleFunc("/options", options)
+	http.HandleFunc("/paymodels", paymodels)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -33,6 +34,20 @@ func home(w http.ResponseWriter, r *http.Request) {
 	</html>`
 	fmt.Fprintf(w, htmlFooter)
 
+}
+
+func paymodels(w http.ResponseWriter, r *http.Request) {
+	userName := r.Header.Get("REMOTE_USER")
+	if payModelExistsForUser(userName) {
+		out, err := json.Marshal(Config.PayModelMap[userName])
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		fmt.Fprintf(w, string(out))
+	} else {
+		http.Error(w, "Not Found", 404)
+	}
 }
 
 func status(w http.ResponseWriter, r *http.Request) {
