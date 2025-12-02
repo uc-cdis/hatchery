cc140f97461e08abe93ec1a548e82e84d76ff070
Jawad <qureshi@uchicago.edu>
2025-12-02 19:11:00 +0100

    set bucket prefix to /

--- hatchery/pods.go
+++ hatchery/pods.go
@@ -407,7 +407,10 @@ func ensureS3PVandPVC(
 ) (pvcName string, err error) {
 
 	pvName, pvcName, volumeHandle := s3NamesForUser(userName)
-	prefix := s3PrefixForUser(userName)
+	// Commented out for now as we don't need a prefix, this bucket will be mounted at / for all users, but read only.
+	// This will be used for the "software-repository" feature
+	// prefix := s3PrefixForUser(userName)
+	prefix := "/"
 
 	// ----- Ensure PV exists (cluster-scoped) -----
 	// Try GET; if not found, create.
