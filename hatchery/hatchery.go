8a4f63c06256bfc13de21ca01c72ba375fcf7e23
Mingfei Shao <mshao1@uchicago.edu>
2026-06-15 14:08:52 -0500

    do not launch workspace if usage is over the soft limit

--- hatchery/hatchery.go
+++ hatchery/hatchery.go
@@ -625,6 +625,10 @@ func launch(w http.ResponseWriter, r *http.Request) {
 			Config.Logger.Printf("Current Paymodel is not set. Launch forbidden for user %s", userName)
 			http.Error(w, "Current Paymodel is not set. Launch forbidden", http.StatusInternalServerError)
 			return
+		} else if payModel.TotalUsage >= payModel.SoftLimit {
+			Config.Logger.Printf("Current Paymodel has reached usage limit (%f/%f). Launch forbidden for user %s", payModel.TotalUsage, payModel.SoftLimit, userName)
+			http.Error(w, "Current Paymodel is has reached usage limit. Launch forbidden", http.StatusInternalServerError)
+			return
 		} else if payModel.Local {
 			err = createLocalK8sPod(r.Context(), hash, userName, accessToken, envVars, payModelId)
 		} else if payModel.Ecs {
